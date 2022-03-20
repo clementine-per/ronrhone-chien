ca7b40c006210c83f7bfc643ecfa91ed1cc64e4a
clementine-per <perreaut.clementine@gmail.com>
2022-03-20 14:19:06 +0100

    Ajout des visites médicales

---
+++ gestion_association/forms/visite_medicale.py
@@ -0,0 +1,53 @@
+from django.db.models import BLANK_CHOICE_DASH
+from django.forms import Form, CharField, ChoiceField, Select, ModelForm, DateField, DateInput
+
+from gestion_association.models.animal import Animal, StatutAnimal
+from gestion_association.models.visite_medicale import TypeVisiteVetoChoice, VisiteMedicale
+
+
+class DateInput(DateInput):
+    input_type = "date"
+
+
+class VisiteMedicaleSearchForm(Form):
+    veterinaire = CharField(max_length=150, required=False)
+    type_visite = ChoiceField(
+        choices=BLANK_CHOICE_DASH + [(tag.name, tag.value) for tag in TypeVisiteVetoChoice],
+        widget=Select(),
+        required=False,
+        label="Type de visite",
+    )
+    date_min = DateField(
+        label="Date de la visite médicale entre le", required=False, widget=DateInput()
+    )
+    date_max = DateField(label=" et le ", required=False, widget=DateInput())
+
+statuts_association_adopte = [
+    StatutAnimal.A_ADOPTER.name,
+    StatutAnimal.ADOPTION.name,
+    StatutAnimal.ADOPTE.name,
+    StatutAnimal.ADOPTABLE.name,
+    StatutAnimal.PEC.name,
+    StatutAnimal.SOCIA.name,
+    StatutAnimal.SOIN.name,
+    StatutAnimal.SEVRAGE.name,
+]
+
+class VisiteMedicaleForm(ModelForm):
+    # Pour mettre les champs obligatoires en gras
+    required_css_class = 'required'
+    class Meta:
+        model = VisiteMedicale
+        fields = (
+            "date",
+            "type_visite",
+            "veterinaire",
+            "commentaire",
+            "montant",
+            "animaux",
+        )
+
+    def __init__(self, *args, **kwargs):
+        super(VisiteMedicaleForm, self).__init__(*args, **kwargs)
+        self.fields['date'].widget.attrs['class'] = 'datePicker'
+        self.fields['animaux'].queryset = Animal.objects.filter(inactif=False).filter(statut__in=statuts_association_adopte)
---
+++ gestion_association/models/visite_medicale.py
@@ -0,0 +1,73 @@
+from dateutil.relativedelta import relativedelta
+from django.db import models
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
+
+from enum import Enum
+
+from gestion_association.models import OuiNonChoice
+from gestion_association.models.animal import TypeVaccinChoice
+
+
+class TypeVisiteVetoChoice(Enum):
+    VAC_PRIMO = "Primo vaccination"
+    VAC_RAPPEL = "Rappel vaccination"
+    STE = "Stérilisation"
+    IDE = "Identification"
+    CONSULT = "Consultation"
+    PACK_STE = "Stérilisation, Identification, primo vaccination"
+    PACK = "Identification, primo vaccination"
+    AUTRE = "Autre"
+    CHIRURGIE = "Chirurgie"
+    URGENCE = "Urgence"
+
+
+class VisiteMedicale(models.Model):
+    date_mise_a_jour = models.DateField(
+        verbose_name="Date de mise à jour", auto_now=True
+    )
+    date = models.DateField(verbose_name="Date de la visite")
+    type_visite = models.CharField(
+        max_length=30,
+        verbose_name="Objet de la visite",
+        choices=[(tag.name, tag.value) for tag in TypeVisiteVetoChoice],
+    )
+    veterinaire = models.CharField(max_length=150, blank=True)
+    commentaire = models.CharField(max_length=2000, blank=True)
+    montant = models.DecimalField(
+        verbose_name="Montant", max_digits=7, decimal_places=2, blank=True, null=True
+    )
+    animaux = models.ManyToManyField("Animal", related_name="visites")
+
+    class Meta:
+        ordering = ["-date"]
+
+    def __str__(self):
+        return f"Visite {self.type_visite} le {self.date} chez {self.veterinaire}"
+
+
+@receiver(m2m_changed, sender=VisiteMedicale.animaux.through)
+def visite_medicale_save_action(sender, instance, **kwargs):
+    # Instance est une visite médicale
+    if instance.type_visite in (
+            TypeVisiteVetoChoice.VAC_PRIMO.name,
+            TypeVisiteVetoChoice.VAC_RAPPEL.name,
+            TypeVisiteVetoChoice.STE.name,
+            TypeVisiteVetoChoice.PACK.name,
+            TypeVisiteVetoChoice.PACK_STE.name,
+    ):
+        for animal in instance.animaux.all():
+            if instance.type_visite in (TypeVisiteVetoChoice.STE.name,TypeVisiteVetoChoice.PACK_STE.name):
+                animal.sterilise = OuiNonChoice.OUI.name
+                animal.date_sterilisation = instance.date
+            if instance.type_visite in (TypeVisiteVetoChoice.VAC_PRIMO.name,
+                                          TypeVisiteVetoChoice.PACK.name, TypeVisiteVetoChoice.PACK_STE.name) :
+                animal.primo_vaccine = OuiNonChoice.OUI.name
+                animal.date_dernier_vaccin = instance.date
+                animal.date_prochain_vaccin = instance.date + relativedelta(weeks=3)
+            if instance.type_visite == TypeVisiteVetoChoice.VAC_RAPPEL.name :
+                animal.primo_vaccine = OuiNonChoice.OUI.name
+                animal.vaccin_ok = OuiNonChoice.OUI.name
+                animal.date_dernier_vaccin = instance.date
+                animal.date_prochain_vaccin = instance.date + relativedelta(months=12)
+            animal.save()
